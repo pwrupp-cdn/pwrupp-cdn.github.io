34e31a515a2233ef2079718f02ac7b94e3697957
blitz313 <blitz313@gmail.com>
2025-01-29 11:55:56 -0500

    updates to branding images

---
+++ snippet-page.php
@@ -0,0 +1,6 @@
+UPDATE TO LINE #20-24 - custom header logo for placement/load
+                else { ?>
+	            	<div class="header-image"></div>
+					<div class="header-image-logo"></div>
+					<div class="header-image-logo-animation"></div>
+	            <?php } ?>
